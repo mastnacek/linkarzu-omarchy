3a1cab6d72856adcd6b34ddf4385af33db52052d
mastnacek <mastnacek@gmail.com>
2026-09-01 17:39:49 +0200

    Update accent to neon green and set dark purple inactive window borders

---
+++ hyprland.lua
@@ -0,0 +1,18 @@
+local active_border_color = "#37f499"
+local inactive_border_color = "rgb(3b2d6b)"
+
+hl.config({
+  general = {
+    col = {
+      active_border = active_border_color,
+      inactive_border = inactive_border_color,
+    },
+  },
+
+  group = {
+    col = {
+      border_active = active_border_color,
+      border_inactive = inactive_border_color,
+    },
+  },
+})
